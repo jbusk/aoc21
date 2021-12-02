9cd5f0b1a11333b42de5c40fece6cdfc7b98f424
Johan Busk Eriksson <johan.busk.eriksson@liu.se>
2021-12-02 12:12:32 +0100

    day 2 pt 1

---
+++ aoc02/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using (var sr = new StreamReader("input.txt"))
+{
+    int hpos = 0;
+    int depth = 0;
+    while (!sr.EndOfStream)
+    {
+        var line = sr.ReadLine()!;
+        var direction = line.Substring(0, line.IndexOf(' '));
+        var value = int.Parse(line.Substring(line.IndexOf(' ')));
+        switch (direction)
+        {
+            case "forward":
+                hpos += value;
+                break;
+            case "down":
+                depth += value;
+                break;
+            case "up":
+                depth -= value;
+                break;
+            default:
+                break;
+        }
+
+    }
+    Console.WriteLine($"hpos {hpos} * depth {depth} = {hpos * depth}");
+}
