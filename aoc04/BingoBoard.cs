f7d1b2328d892b1f1fe455b2c42f3beefd879bb8
Johan Busk Eriksson <busk@lysator.liu.se>
2021-12-04 13:05:17 +0100

    Day 4 parts 1 and 2

---
+++ aoc04/BingoBoard.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc04
+{
+    internal class BingoBoard
+    {
+        public BingoBoard(string board)
+        {
+            var grid = new List<GameField>();
+            foreach (var item in System.Text.RegularExpressions.Regex.Split(board, @"\s+"))
+            {
+                int value = int.Parse(item);
+                grid.Add(new GameField(int.Parse(item)));
+            }
+            Grid = grid;
+        }
+        public List<GameField> Grid { get; set; }
+        public bool Won
+        {
+            get
+            {
+                // check vertical
+                for (int i = 0; i < 5; i++)
+                {
+                    if (Grid[i].Called && Grid[i + 5].Called && Grid[i + 10].Called && Grid[i + 15].Called && Grid[i + 20].Called)
+                        return true;
+                }
+                // check horizontal
+                for (int i = 0; i < 25; i = i + 5)
+                {
+                    if (Grid[i].Called && Grid[i + 1].Called && Grid[i + 2].Called && Grid[i + 3].Called && Grid[i + 4].Called)
+                        return true;
+                }
+                return false;
+            }
+        }
+
+        public bool Call(int number)
+        {
+            var gameField = Grid.Find(x => x.Value == number);
+            if (gameField == null)
+                return false;
+            gameField.Called = true;
+            return Won;
+        }
+
+        public int Sum
+        {
+            get
+            {
+                return Grid.FindAll(x => !x.Called).Sum(u => u.Value);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retval = "";
+            for (int i = 0; i < 25; i = i + 5)
+            {
+                retval += $"{Grid[i]} {Grid[i + 1]} {Grid[i + 2]} {Grid[i + 3]} {Grid[i + 4]}\n";
+            }
+            return retval;
+        }
+
+    }
+
+    internal class GameField
+    {
+        public GameField(int value)
+        {
+            Value = value;
+            Called = false;
+        }
+        public int Value { get; set; }
+        public bool Called { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value}" + (Called ? "!" : "");
+        }
+    }
+}
---
+++ aoc04/Program.cs
@@ -0,0 +1,45 @@
+﻿using aoc04;
+
+List<BingoBoard> boards = new();
+string calls;
+using (var sr = new StreamReader("input.txt"))
+{
+    calls = sr.ReadLine()!;
+    while (!sr.EndOfStream)
+    {
+        sr.ReadLine();
+        var line1 = sr.ReadLine()! + " ";
+        var line2 = sr.ReadLine()! + " ";
+        var line3 = sr.ReadLine()! + " ";
+        var line4 = sr.ReadLine()! + " ";
+        var line5 = sr.ReadLine()!;
+        boards.Add(new BingoBoard($"{line1 + line2 + line3 + line4 + line5}".Trim()));
+    }
+}
+
+bool first_has_won = false;
+BingoBoard? last_winning = null;
+int last_winning_sum = 0;
+int last_winning_call = 0;
+foreach (var callstr in calls.Split(','))
+{
+    int call = int.Parse(callstr);
+    foreach (var board in boards)
+    {
+        if (!board.Won && board.Call(call))
+        {
+            int sum = board.Sum;
+            if (!first_has_won)
+            {
+                Console.WriteLine($"Part 1: {sum} * {call} = {sum * call}");
+                Console.WriteLine(board);
+                first_has_won = true;
+            }
+            last_winning = board;
+            last_winning_call = call;
+            last_winning_sum = sum;
+        }
+    }
+}
+Console.WriteLine($"Part 2: {last_winning_sum} * {last_winning_call} = {last_winning_sum * last_winning_call}");
+Console.WriteLine(last_winning);
