dd8f9986ba4acb1d2e8ced6d9240464736fbfd8d
Johan Busk Eriksson <busk@lysator.liu.se>
2021-12-04 13:43:26 +0100

    Cleanup

--- aoc04/BingoBoard.cs
+++ aoc04/BingoBoard.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace aoc04
+﻿namespace aoc04
 {
     internal class BingoBoard
     {
