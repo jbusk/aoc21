6bb0993871b53285f53baafbedc6babfa8681de6
Johan Busk Eriksson <busk@lysator.liu.se>
2021-12-05 17:18:19 +0100

    New solution for day 5

--- aoc05/Program.cs
+++ aoc05/Program.cs
@@ -1,32 +1,18 @@
-﻿var vents = new int[1000,1000];
+﻿var vents = new int[1000, 1000];
 using (var sr = new StreamReader("input.txt"))
 {
     while (!sr.EndOfStream)
     {
         var line = sr.ReadLine()!.Split(" -> ");
-        (var beg, var end) = (line[0].Split(','), line[1].Split(',').ToArray());
-        (var x1, var x2, var y1, var y2) = (int.Parse(beg[0]), int.Parse(end[0]), int.Parse(beg[1]), int.Parse(end[1]));
-        
-        if (x1 == x2 || y1 == y2)
+        (string[] point1, string[] point2) = (line[0].Split(','), line[1].Split(',').ToArray());
+        (int x1, int x2, int y1, int y2) = (int.Parse(point1[0]), int.Parse(point2[0]), int.Parse(point1[1]), int.Parse(point2[1]));
+        int xstep = x1 == x2 ? 0 : (x1 < x2 ? 1 : -1);
+        int ystep = y1 == y2 ? 0 : (y1 < y2 ? 1 : -1);
+        int stopx = x2 + (xstep == 0 ? 1 : xstep);
+        int stopy = y2 + (ystep == 0 ? 1 : ystep);
+        for (int x = x1, y = y1; x != stopx && y != stopy; x += xstep, y += ystep)
         {
-            (int xdiff, int ydiff) = (x1 - x2, y1 - y2);
-            for (int x = 0; x <= Math.Abs(xdiff); x++)
-            {
-                for (int y = 0; y <= Math.Abs(ydiff); y++)
-                {
-                    (var xc, var yc) = (x1 + ((xdiff < 0) ? x : x * -1), y1 + ((ydiff < 0) ? y : y * -1));
-                    vents[xc, yc]++; 
-                }
-            }
-        }
-        else
-        {
-            (var diff, var xdir, var ydir) = (Math.Abs(x1 - x2), x1 - x2, y1 - y2);
-            for (int xy = 0; xy <= diff; xy++)
-            {
-                (var xc, var yc) = (x1 + ((xdir < 0) ? xy : xy * -1), y1 + ((ydir < 0) ? xy : xy * -1));
-                vents[xc, yc]++;
-            }
+            vents[x, y]++;
         }
     }
 }
