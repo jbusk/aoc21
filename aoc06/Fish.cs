5217d746ead05922d73c183fb3416b48e4870c41
Johan Busk Eriksson <johan.busk.eriksson@liu.se>
2021-12-06 10:46:00 +0100

    Day 6 parts 1 and 2

---
+++ aoc06/Fish.cs
@@ -0,0 +1,43 @@
+﻿namespace aoc06
+{
+    internal class Fish
+    {
+        public Fish()
+        {
+            Counter = 8;
+            Amount = 1;
+        }
+        public Fish(int counter)
+        {
+            Counter = counter;
+            Amount = 1;
+        }
+        public Fish(int counter, long amount)
+        {
+            Counter=counter;
+            Amount=amount;
+        }
+        public int Counter { get; set; }
+
+        public long Amount { get; set; }
+        public long Day()
+        {
+            if (Counter == 0)
+            {
+                Counter = 6;
+                return Amount;
+            }
+            Counter--;
+            return 0;
+        }
+        public void AddAmount(int amount)
+        {
+            Amount += amount;
+        }
+
+        public override string ToString()
+        {
+            return $"({Amount},{Counter})";
+        }
+    }
+}
---
+++ aoc06/Program.cs
@@ -0,0 +1,36 @@
+﻿using aoc06;
+
+List<Fish> fishes = new();
+var input = File.ReadAllText("input.txt").Split(',').Select(x => int.Parse(x));
+foreach (var item in input)
+{
+    var fisk = fishes.Find(x => x.Counter == item);
+    if (fisk == null)
+        fishes.Add(new Fish(item));
+    else
+        fisk.AddAmount(1);
+}
+
+for (int i = 0; i < 256; i++)
+{
+    long fishtoadd = 0;
+    foreach (var fish in fishes)
+    {
+        fishtoadd += fish.Day();
+    }
+    fishes.Add(new Fish(8, fishtoadd));
+    List<Fish> school = new();
+    for (int j = 0; j <= 8; j++)
+    {
+        long num = fishes.FindAll(x=> x.Counter == j).Sum(x => x.Amount);
+        school.Add(new Fish(j, num));
+    }
+    fishes = school;
+
+    if (i == 79)
+    {
+        Console.WriteLine("Part 1: " + fishes.Sum(x => x.Amount));
+    }
+}
+
+Console.WriteLine("Part 2: " + fishes.Sum(x => x.Amount));
