e7ffd7ff64a325d259910f2fe9f66b72325c4d1a
Johan Busk Eriksson <busk@lysator.liu.se>
2021-12-07 11:45:33 +0100

    Use median and mean instead for day 7

--- aoc07/Program.cs
+++ aoc07/Program.cs
@@ -1,30 +1,23 @@
-﻿using aoc07;
+﻿var input = File.ReadAllText("input.txt").Split(',').Select(x => int.Parse(x));
 
-var input = File.ReadAllText("input.txt").Split(',').Select(x => int.Parse(x));
+Console.WriteLine($"Part 1: {cost1(input)}");
+Console.WriteLine($"Part 2: {cost2(input)}");
 
-List<Crab> crabs = new();
-foreach (var pos in input)
+int median(IEnumerable<int> values)
 {
-    var crab = crabs.Find(x => x.Position == pos);
-    if (crab == null)
-        crabs.Add(new Crab(pos));
-    else
-        crab.Amount++;
+    var arry = values.ToArray();
+    return arry[arry.Length / 2];
 }
 
-int lowest = crabs.MinBy(x => x.Position)!.Position;
-int highest = crabs.MaxBy(x => x.Position)!.Position;
-int pt1cheapest = int.MaxValue;
-int pt2cheapest = int.MaxValue;
-for (int i = lowest; i < highest; i++)
+int cost1(IEnumerable<int> values)
 {
-    int pt1cost = crabs.Sum(x => x.Pt1Align(i));
-    int pt2cost = crabs.Sum(x => x.Pt2Align(i));
-    if (pt1cost < pt1cheapest)
-        pt1cheapest = pt1cost;
-    if (pt2cost < pt2cheapest)
-        pt2cheapest = pt2cost;
+    int position = median(values);
+    return Math.Min(values.Sum(x => Math.Abs(x - position)), values.Sum(x => Math.Abs(x - position + 1)));
 }
 
-Console.WriteLine($"Part 1: {pt1cheapest}");
-Console.WriteLine($"Part 2: {pt2cheapest}");
+int cost2(IEnumerable<int> values)
+{
+    var ceil = (int)values.Average();
+    var floor = ceil + 1;
+    return Math.Min(values.Sum(x => (Math.Abs(x - ceil) * (Math.Abs(x - ceil) +1) /2)), values.Sum(x => Math.Abs(x - floor) * (Math.Abs(x - floor) + 1) / 2));
+}
