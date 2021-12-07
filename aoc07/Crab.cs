b8e20c988b677c6d6093bca73add9ecb76820048
Johan Busk Eriksson <johan.busk.eriksson@liu.se>
2021-12-07 10:42:16 +0100

    Day 7 pt 1 and 2

---
+++ aoc07/Crab.cs
@@ -0,0 +1,27 @@
+﻿namespace aoc07
+{
+    internal class Crab
+    {
+        public Crab(int position)
+        {
+            Position = position;
+            Amount = 1;
+        }
+
+        public int Position { get; set; }
+        public int Amount { get; set; }
+        public int Pt1Align(int requested)
+        {
+            return Amount * Math.Abs(Position - requested);
+        }
+        public int Pt2Align(int requested)
+        {
+            int diff = Math.Abs(Position - requested);
+            return Amount * (diff * (diff + 1)/2);
+        }
+        public override string ToString()
+        {
+            return $"({Position}:{Amount})";
+        }
+    }
+}
---
+++ aoc07/Program.cs
@@ -0,0 +1,30 @@
+﻿using aoc07;
+
+var input = File.ReadAllText("input.txt").Split(',').Select(x => int.Parse(x));
+
+List<Crab> crabs = new();
+foreach (var pos in input)
+{
+    var crab = crabs.Find(x => x.Position == pos);
+    if (crab == null)
+        crabs.Add(new Crab(pos));
+    else
+        crab.Amount++;
+}
+
+int lowest = crabs.MinBy(x => x.Position)!.Position;
+int highest = crabs.MaxBy(x => x.Position)!.Position;
+int pt1cheapest = int.MaxValue;
+int pt2cheapest = int.MaxValue;
+for (int i = lowest; i < highest; i++)
+{
+    int pt1cost = crabs.Sum(x => x.Pt1Align(i));
+    int pt2cost = crabs.Sum(x => x.Pt2Align(i));
+    if (pt1cost < pt1cheapest)
+        pt1cheapest = pt1cost;
+    if (pt2cost < pt2cheapest)
+        pt2cheapest = pt2cost;
+}
+
+Console.WriteLine($"Part 1: {pt1cheapest}");
+Console.WriteLine($"Part 2: {pt2cheapest}");
