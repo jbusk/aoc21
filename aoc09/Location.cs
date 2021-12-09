3ed7f50380d299137855ec782dc0e4d5003541fd
Johan Busk Eriksson <busk@lysator.liu.se>
2021-12-09 10:31:54 +0100

    pt2

---
+++ aoc09/Location.cs
@@ -0,0 +1,25 @@
+﻿namespace aoc09
+{
+    internal class Location
+    {
+        public Location(int depth, int north, int south, int west, int east)
+        {
+            Depth = depth;
+            North = north;
+            South = south;
+            West = west;
+            East = east;
+            if (depth == 9)
+                Basin = false;
+            else
+                Basin = true;
+        }
+        public int Depth { get; set; }
+        public int North { get; set; }
+        public int South { get; set; }
+        public int West { get; set; }
+        public int East { get; set; }
+
+        public bool Basin { get; set; }
+    }
+}
--- aoc09/Program.cs
+++ aoc09/Program.cs
@@ -1,35 +1,44 @@
-﻿string? first = null ;
-string? second = null;
-string? current = null;
-int pt1sum = 0;
-using (var sr = new StreamReader("input.txt"))
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllLines("input.txt");
+var grid = input.Select(l => l.ToArray()).ToArray();
+
+IEnumerable<(int y, int x)> adjacent(int y, int x)
 {
-    bool lastline = false;
-    while (!sr.EndOfStream)
+    if (x > 0) yield return (y, x - 1);
+    if (x < grid[0].Length - 1) yield return (y, x + 1);
+    if (y > 0) yield return (y - 1, x);
+    if (y < grid.Length - 1) yield return (y + 1, x);
+}
+var lows = new List<(int y, int x)>();
+for (int y = 0; y < grid.Length; ++y)
+{
+    for (int x = 0; x < grid[0].Length; ++x)
     {
-        current = sr.ReadLine()!;
-        if (string.IsNullOrEmpty(current))
-            lastline = true;
-        if (second != null)
-        {
-            for (int i = 0; i < second.Length; i++)
-            {
-                if (lastline || second[i] < current[i]) {
-                    if (first == null || second[i] < first[i])
-                    {
-                        if ((i == 0 || second[i] < second[i-1]))
-                        {
-                            if ((i == second.Length -1 || second[i] < second[i+1]))
-                            {
-                                pt1sum += 1 + int.Parse(second[i].ToString());
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        first = second;
-        second = current;
+        if (adjacent(y, x).All(a => grid[a.y][a.x] > grid[y][x])) lows.Add((y, x));
     }
 }
-Console.WriteLine("Part 1: " + pt1sum);
+
+var part1 = lows.Sum(a => int.Parse($"{grid[a.y][a.x]}")) + lows.Count;
+Console.WriteLine($"Part 1: {part1}");
+
+var visited = new HashSet<(int y, int x)>();
+IEnumerable<(int y, int x)> basin((int y, int x) low)
+{
+    if (!visited.Contains(low) && grid[low.y][low.x] != '9')
+    {
+        visited.Add(low);
+        yield return low;
+
+        foreach (var a in adjacent(low.y, low.x))
+            foreach (var c in basin(a))
+                yield return c;
+    }
+}
+var basins = lows.Select(l => basin(l).ToArray()).OrderByDescending(b => b.Length).ToArray();
+
+var part2 = basins.Take(3).Aggregate(1L, (s, b) => s * b.Count());
+Console.WriteLine($"Part 2: {part2}");
