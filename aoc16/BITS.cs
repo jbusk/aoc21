3a179af4f22908b8a5b309dfe821b9d1b3174985
Johan Busk Eriksson <busk@lysator.liu.se>
2021-12-16 14:11:46 +0100

    fy fan

---
+++ aoc16/BITS.cs
@@ -0,0 +1,39 @@
+﻿namespace aoc16
+{
+    internal class BITS
+    {
+        public BITS(string binary)
+        {
+            Binary = binary;
+        }
+        public string Binary { get; set; }
+        public int Version { get { return Three(Binary.Substring(0, 3)); } }
+
+        public int TypeID { get { return Three(Binary.Substring(3, 3)); } }
+
+        private int Three(string param)
+        {
+            switch (param)
+            {
+                case "000":
+                    return 0;
+                case "001":
+                    return 1;
+                case "010":
+                    return 2;
+                case "011":
+                    return 3;
+                case "100":
+                    return 4;
+                case "101":
+                    return 5;
+                case "110":
+                    return 6;
+                case "111":
+                    return 7;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
